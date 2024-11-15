75ac68e25c1746a028c33b6430e61ae88e5578c6
Xo Wang <xo@shapertools.com>
2024-11-15 14:25:50 -0800

    Customize for ShaperTools/Tool

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 cd "$GITHUB_WORKSPACE" || exit
 
-find . \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec cmake-format $* {} +
+find . -type d \( -path ./build\* -o -path ./external \) -prune -false -o -type f \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec cmake-format $* {} +
 
 exit $?
